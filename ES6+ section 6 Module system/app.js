8db232d78276c45f437939fa18d7910a74e5ae6e
ShoaibTamboli <shoaibtamboli03@gmail.com>
2022-01-12 23:59:10 +0530

    update: section 6 completed

--- ES6+ section 6 Module system/app.js
+++ ES6+ section 6 Module system/app.js
@@ -1,7 +1,31 @@
 console.log("Section 6: Module System");
 
 /*
+example 20:
+Topic: default export
 
+//  →  /utils.js
 
+const square = (a) => a * a
+export default square
+
+//  →  /app.js
+import square from './utils'
+
+console.log(square(2))
+
+example 21:
+Topic: import/export with rename
+//  →  /utils.js
+
+const add = (x, y) => x + y
+
+export { add as sum }
+
+//  →  /app.js
+
+import { sum as xyz } from './utils'
+
+console.log(xyz(2, 5))
 
 */
